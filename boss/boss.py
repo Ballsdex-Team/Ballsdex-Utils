efd31883d0daeaf11a2cc9f85a6cfabd37696e28
flaree <flare2399@gmail.com>
2024-01-17 18:40:56 +0000

    add auto round hosting of boss

--- boss/boss.py
+++ boss/boss.py
@@ -1,5 +1,6 @@
 import logging
 import random
+import asyncio
 import sys
 from typing import TYPE_CHECKING
 
@@ -7,7 +8,7 @@ import discord
 from redbot.core import commands, app_commands
 
 from ballsdex import __version__ as ballsdex_version
-from ballsdex.core.models import Ball, BallInstance
+from ballsdex.core.models import Ball, BallInstance, Blacklist
 from ballsdex.core.models import balls as countryballs
 from ballsdex.core.utils.tortoise import row_count_estimate
 from ballsdex.settings import settings
@@ -20,20 +21,21 @@ if TYPE_CHECKING:
 
 log = logging.getLogger("ballsdex.packages.boss")
 
-
+roles = [1049119446372986921]
 class BossView(View):
-    def __init__(self, interaction: discord.Interaction, entry_list, dead_list):
+    def __init__(self, interaction: discord.Interaction, entry_list, dead_list, joinable):
         super().__init__(timeout=300)
         self.value = None
         self.interaction = interaction
         self.entry_list = entry_list
         self.dead_list = dead_list
+        self.joinable = joinable
 
     async def on_timeout(self):
         for item in self.children:
             item.disabled = True  # type: ignore
         try:
-            await self.interaction.followup.edit_message("@original", view=self)  # type: ignore
+            await self.interaction.response.send_message("@original", view=self)  # type: ignore
         except discord.NotFound:
             pass
 
@@ -41,17 +43,23 @@ class BossView(View):
         style=discord.ButtonStyle.success, label="Join"
     )
     async def join_button(self, interaction: discord.Interaction, button: Button):
+        if not self.joinable:
+            await interaction.response.send_message("The boss battle is not joinable.", ephemeral=True)
+            return
+        if await Blacklist.filter(discord_id=interaction.user.id).exists():
+            await interaction.response.send_message("You are blacklisted and cannot join the boss battle.", ephemeral=True)
+            return
         if (interaction.user.id, interaction.user.display_name) in self.dead_list:
-            await interaction.followup.send(f"{interaction.user.mention} is dead and cannot rejoin.", ephemeral=True)
+            await interaction.response.send_message(f"{interaction.user.mention} is dead and cannot rejoin.", ephemeral=True)
             return
         if (interaction.user.id, interaction.user.display_name) in self.entry_list:
             self.entry_list.remove((interaction.user.id, interaction.user.display_name) )
             # reply to interaction
-            await interaction.followup.send(f"{interaction.user.mention} has left the boss battle.", ephemeral=True)
+            await interaction.response.send_message(f"{interaction.user.mention} has left the boss battle.", ephemeral=True)
         else:
             self.entry_list.append((interaction.user.id, interaction.user.display_name) )
             # reply to interaction
-            await interaction.followup.send(f"{interaction.user.mention} has joined the boss battle.", ephemeral=True)
+            await interaction.response.send_message(f"{interaction.user.mention} has joined the boss battle.", ephemeral=True)
         
         
 
@@ -62,27 +70,45 @@ class Boss(commands.Cog):
 
     def __init__(self, bot: "BallsDexBot"):
         self.bot = bot
+        self.joinable = False
         self.boss = None
         self.boss_hp = 0
         self.boss_max_hp = 0
         self.boss_entries = []
         self.boss_dead = []
+        self.message = None
 
 
     boss = app_commands.Group(name="boss", description="Boss management", guild_ids=[1049118743101452329])
 
     @boss.command()
-    @app_commands.checks.has_any_role(*settings.root_role_ids)
+    @app_commands.checks.has_any_role(*roles)
     async def start(self, interaction: discord.Interaction):
         """
         Start a boss.
         """
         # Create a message with a button that when pressed, adds you to boss_entries
-        view = BossView(interaction, self.boss_entries, self.boss_dead)
-        await interaction.response.send_message("Boss battle started!", ephemeral=True, view=view)
+        if self.boss is None:
+            await interaction.response.send_message("There is no boss chosen.", ephemeral=True)
+            return
+        while self.boss_hp > 0:
+            self.joinable = True
+            view = BossView(interaction, self.boss_entries, self.boss_dead, self.joinable)
+            message = await interaction.response.send_message("Boss round started!", view=view)
+            self.message = message
+            await asyncio.sleep(300)
+            self.joinable = False
+            if random.randint(0, 100) < 90:
+                # 20 of boss entries die
+                await self.end_round(interaction, random.randint(0, len(self.boss_entries) / 5))
+            else:
+                await self.end_round(interaction)
+            await asyncio.sleep(5)
+            
+
     
     @boss.command()
-    @app_commands.checks.has_any_role(*settings.root_role_ids)
+    @app_commands.checks.has_any_role(*roles)
     async def choose(self, interaction: discord.Interaction):
         """
         Choose a boss.
@@ -109,7 +135,7 @@ class Boss(commands.Cog):
         # Choose a random boss
         boss = random.choice(bosses)
         #choose a random hp
-        hp = random.randint(75000, 25000)
+        hp = random.randint(75000, 250000)
         # Set the boss
         self.boss = boss
         # Set the boss hp
@@ -134,32 +160,98 @@ class Boss(commands.Cog):
         return base
 
     @boss.command()
-    @app_commands.checks.has_any_role(*settings.root_role_ids)
-    async def endround(self, interaction: discord.Interaction):
+    @app_commands.checks.has_any_role(*roles)
+    async def endround(self, interaction: discord.Interaction, killed: int = 0):
         """
         End the round.
         """
-        await interaction.response.defer(thinking=True, ephemeral=True)
+        # End the round
+        await self.end_round(interaction, killed)
+
+    async def end_round(self, interaction: discord.Interaction, killed: int = 0):
+        self.joinable = False
+        await interaction.response.defer(thinking=True)
+        defeated = False
         # loop through the entries and pick a random ball from each entry to attack the boss
         attack_msg = "The boss has been attacked! The following balls have attacked the boss: \n"
         for entry in self.boss_entries:
             # Choose a random ball from the entry
-            balls = await BallInstance.filter(player__discord_id=entry)
+            balls = await BallInstance.filter(player__discord_id=entry[0]).prefetch_related("ball")
             ball = random.choice(balls)
+            user = interaction.guild.get_member(entry[0])
+            if user is None:
+                user = await self.bot.fetch_user(entry[0])
             # Get the ball's attack
             attack = self.get_bonus(ball)
             # Subtract the attack from the boss hp
             self.boss_hp -= attack
             # Add the ball to the attack message
-            attack_msg += f"{ball.name} attacked the boss for {attack} damage!\n"
+            attack_msg += f"{user.display_name}'s {ball.ball} attacked the boss for {attack} damage!\n"
             if self.boss_hp <= 0:
-                attack_msg += f"The boss has been defeated! {ball.name} has won the boss battle, this ball was played by <@{entry}>!"
+                defeated = user
+                attack_msg += f"The boss has been defeated! {ball.ball} has won the boss battle, this ball was played by {user.display_name} ({entry[0]})!"
                 break
         # Send the attack message
         file = BytesIO(attack_msg.encode("utf-8"))
-        await interaction.response.followup(file=discord.File(file, "attack.txt"))
+        if defeated:
+            await interaction.followup.send(file=discord.File(file, "attack.txt"), content=f"{defeated.mention} has won the boss battle!")
+        else:
+            content = ""
+            if killed > 0:
+                # random from entries, add to dead list
+                dead_list = []
+                for i in range(killed):
+                    dead = random.choice(self.boss_entries)
+                    self.boss_entries.remove(dead)
+                    self.boss_dead.append(dead)
+                    dead_list.append(dead)
+                # add to content for each dead
+                content += f"{killed} people have died!\n"
+                content += "The following people have died:\n"
+                for dead in dead_list:
+                    content += f"<@{dead[0]}>\n"
+            await interaction.followup.send(content=content, file=discord.File(file, "attack.txt"))
+            # reset entries
+            self.boss_entries = []
+            self.joinable = True
 
-        
+
+    @boss.command()
+    @app_commands.checks.has_any_role(*roles)
+    async def stats(self, interaction: discord.Interaction):
+        """
+        Get the stats of the boss.
+        """
+        await interaction.response.defer(thinking=True, ephemeral=True)
+        # Send a message with the boss's stats
+        await interaction.followup.send(f"The boss is {self.boss} and has {self.boss_hp}/{self.boss_max_hp} hp left.")
+    
+    @boss.command()
+    @app_commands.checks.has_any_role(*roles)
+    async def info(self, interaction: discord.Interaction):
+        """
+        Get the stats of the boss.
+        """
+        await interaction.response.defer(thinking=True, ephemeral=True)
+        # Send a message with the how many are enterted and how many are dead
+        await interaction.followup.send(f"There are {len(self.boss_entries)} balls entered and {len(self.boss_dead)} balls dead.")
+
+    @boss.command()
+    @app_commands.checks.has_any_role(*roles)
+    async def reset(self, interaction: discord.Interaction):
+        """
+        Reset the boss.
+        """
+        await interaction.response.defer(thinking=True, ephemeral=True)
+        # Reset the boss
+        self.boss = None
+        self.boss_hp = 0
+        self.boss_max_hp = 0
+        self.boss_entries = []
+        self.boss_dead = []
+        self.joinable = False
+        # Send a message saying the boss has been reset
+        await interaction.followup.send(f"The boss has been reset.")
             
 
 
